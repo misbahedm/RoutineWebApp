1dbd894a8ac4fb55c51222921acb507caf08cdc0
misbahedm <misbah129@gmail.com>
2019-08-24 07:47:07 +0600

    routine webview app version beta

---
+++ app/src/main/java/misbahedm/github/io/MainActivity.java
@@ -0,0 +1,36 @@
+package misbahedm.github.io;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView wv;
+    String url="https://misbahedm.github.io/";
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        wv = (WebView) findViewById(R.id.webView);
+        wv.setWebViewClient(new WebViewClient());
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.loadUrl(url);
+
+
+
+
+
+    }
+}
